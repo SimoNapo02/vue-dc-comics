dbd27aba33dac84d880d1d39a0891c61e5026e2c
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-06-03 13:07:39 +0200

    start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <BaseHeader/>
+   <BaseMain/>
+   <BaseFooter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import baseHeader from './components/baseHeader.vue';
+import baseMain from './components/baseMain.vue';
+import baseFooter from './components/baseFooter.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-  },
+   components: {
+    baseHeader,
+    baseMain,
+    baseFooter
+}
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import './assets/style/global.scss';
+
 </style>
